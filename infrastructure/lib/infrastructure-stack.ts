7d60e5a5e83ba936410e254e01f8e466fe5cd906
Alexandre De Riemaecker <riemaecker@gmail.com>
2025-01-13 12:41:50 +0100

    upserts data after neptune creation via custom resource

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as cr from "aws-cdk-lib/custom-resources";
 import { Construct } from "constructs";
 import * as neptune from "@aws-cdk/aws-neptune-alpha";
 
@@ -45,5 +48,74 @@ export class InfrastructureStack extends cdk.Stack {
         clusterParameterGroup: neptuneParameterGroup,
       }
     );
+
+    // Hook a Lambda to neptune cluster created/updated events to upsert default data into the neptune database
+    this.upsertDataAfterCreateOrUpdate(vpc, neptuneCluster);
+  }
+
+  /**
+   * This method creates a Lambda function to upsert default data into the Neptune database
+   * when the Neptune cluster is created or updated.
+   *
+   * @param vpc - The VPC in which the Neptune cluster is deployed.
+   * @param neptuneCluster - The Neptune database cluster.
+   */
+  private upsertDataAfterCreateOrUpdate(
+    vpc: ec2.Vpc,
+    neptuneCluster: neptune.DatabaseCluster
+  ) {
+    // Create a Lambda function to initialize the Neptune database with default data
+    const initLambda = new lambda.Function(
+      this,
+      "BankAccountService-InitNeptuneLambda",
+      {
+        description:
+          "Upserts default data into neptune db on create/update",
+        code: lambda.Code.fromAsset(
+          "../services/account-balance-importdata-oncreate/src"
+        ),
+        handler: "index.handler",
+        runtime: lambda.Runtime.NODEJS_22_X,
+        timeout: cdk.Duration.minutes(2),
+        vpc,
+      }
+    );
+
+    // Grant Lambda permission to run Gremlin queries with the neptune data API
+    initLambda.role?.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: ["neptunedata:ExecuteGremlinQuery"],
+        resources: [
+          `arn:aws:neptunedata:${this.region}:${this.account}:cluster:${neptuneCluster.clusterIdentifier}`,
+        ],
+      })
+    );
+
+    // Allow Lambda to connect to Neptune’s port if needed
+    neptuneCluster.connections.allowDefaultPortFrom(initLambda);
+
+    // Custom resource provider for lambda
+    const provider = new cr.Provider(
+      this,
+      "BankAccountService-InitNeptuneProvider",
+      {
+        onEventHandler: initLambda,
+      }
+    );
+
+    // Custom Resource to trigger the Lambda on create/update
+    const initCustomResource = new cdk.CustomResource(
+      this,
+      "BankAccountService-InitNeptuneCustomResource",
+      {
+        serviceToken: provider.serviceToken,
+        properties: {
+          NeptuneEndpointHostname: neptuneCluster.clusterEndpoint.hostname,
+          NeptuneEndpointPort: neptuneCluster.clusterEndpoint.port.toString(),
+        },
+      }
+    );
+
+    initCustomResource.node.addDependency(neptuneCluster);
   }
 }
