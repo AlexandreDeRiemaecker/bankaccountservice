fd3b544a58af58429e027207507e9be6103a2a4b
Alexandre De Riemaecker <riemaecker@gmail.com>
2025-01-14 12:04:37 +0100

    implements the tests for BankTransaction Service and Controller

--- services/bankaccount-service-api/src/bank-transactions/bank-transactions.controller.spec.ts
+++ services/bankaccount-service-api/src/bank-transactions/bank-transactions.controller.spec.ts
@@ -2,9 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BankTransactionsController } from './bank-transactions.controller';
 import { BankTransactionsService } from './bank-transactions.service';
 import { SharedModule } from '../shared/shared.module';
+import { CreateBankTransactionDto } from './dto/create-bank-transaction.dto';
+import { BankTransaction } from './entities/bank-transaction.entity';
+import { MockFunctionMetadata, ModuleMocker } from 'jest-mock';
 
 describe('BankTransactionsController', () => {
   let controller: BankTransactionsController;
+  let service: BankTransactionsService;
 
   beforeAll(() => {
     process.env.NEPTUNE_ENDPOINT_HOSTNAME = 'localhost';
@@ -12,18 +16,157 @@ describe('BankTransactionsController', () => {
   });
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleMocker = new ModuleMocker(global);
+
+    const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [SharedModule],
       controllers: [BankTransactionsController],
-      providers: [BankTransactionsService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === BankTransactionsService) {
+          return {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
-    controller = module.get<BankTransactionsController>(
+    controller = moduleRef.get<BankTransactionsController>(
       BankTransactionsController,
     );
+    service = moduleRef.get<BankTransactionsService>(BankTransactionsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a bank transaction', async () => {
+      const createBankTransactionDto: CreateBankTransactionDto = {
+        otherPersonIBAN: 'DE1234567890',
+        amount: 1000,
+      };
+      const result: BankTransaction = {
+        otherPersonIBAN: 'DE1234567890',
+        amount: 1000,
+      };
+      jest.spyOn(service, 'create').mockResolvedValue(result);
+
+      expect(await controller.create(createBankTransactionDto)).toBe(result);
+    });
+
+    it('should throw an error if creation fails', async () => {
+      const createBankTransactionDto: CreateBankTransactionDto = {
+        otherPersonIBAN: 'DE1234567890',
+        amount: 1000,
+      };
+      jest
+        .spyOn(service, 'create')
+        .mockRejectedValue(new Error('Creation failed'));
+
+      await expect(controller.create(createBankTransactionDto)).rejects.toThrow(
+        'Failed to create bank transaction',
+      );
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of bank transactions', async () => {
+      const result: BankTransaction[] = [
+        {
+          otherPersonIBAN: 'DE1234567890',
+          amount: 1000,
+        },
+      ];
+      jest.spyOn(service, 'findAll').mockResolvedValue(result);
+
+      expect(await controller.findAll()).toBe(result);
+    });
+
+    it('should throw an error if retrieval fails', async () => {
+      jest
+        .spyOn(service, 'findAll')
+        .mockRejectedValue(new Error('Retrieval failed'));
+
+      await expect(controller.findAll()).rejects.toThrow(
+        'Failed to retrieve bank transactions',
+      );
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single bank transaction', async () => {
+      const result: BankTransaction = {
+        otherPersonIBAN: 'DE1234567890',
+        amount: 1000,
+      };
+      jest.spyOn(service, 'findOne').mockResolvedValue(result);
+
+      expect(await controller.findOne('transactionId')).toBe(result);
+    });
+
+    it('should throw an error if bank transaction is not found', async () => {
+      jest
+        .spyOn(service, 'findOne')
+        .mockRejectedValue(new Error('Failed to retrieve bank transaction'));
+
+      await expect(controller.findOne('transactionId')).rejects.toThrow(
+        'Failed to retrieve bank transaction',
+      );
+    });
+  });
+
+  describe('update', () => {
+    it('should update a bank transaction', async () => {
+      const updateBankTransactionDto = { amount: 2000 };
+      const result = { updatedVertexId: 'updatedVertexId' };
+      jest.spyOn(service, 'update').mockResolvedValue(result);
+
+      expect(
+        await controller.update('transactionId', updateBankTransactionDto),
+      ).toBe(result);
+    });
+
+    it('should throw an error if update fails', async () => {
+      const updateBankTransactionDto = { amount: 2000 };
+      jest
+        .spyOn(service, 'update')
+        .mockRejectedValue(new Error('Update failed'));
+
+      await expect(
+        controller.update('transactionId', updateBankTransactionDto),
+      ).rejects.toThrow('Failed to update bank transaction');
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a bank transaction', async () => {
+      const result = { deletedVertexId: 'deletedVertexId' };
+      jest.spyOn(service, 'remove').mockResolvedValue(result);
+
+      expect(await controller.remove('transactionId')).toBe(result);
+    });
+
+    it('should throw an error if deletion fails', async () => {
+      jest
+        .spyOn(service, 'remove')
+        .mockRejectedValue(new Error('Deletion failed'));
+
+      await expect(controller.remove('transactionId')).rejects.toThrow(
+        'Failed to delete bank transaction',
+      );
+    });
   });
 });
--- services/bankaccount-service-api/src/bank-transactions/bank-transactions.service.spec.ts
+++ services/bankaccount-service-api/src/bank-transactions/bank-transactions.service.spec.ts
@@ -1,9 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BankTransactionsService } from './bank-transactions.service';
 import { SharedModule } from '../shared/shared.module';
+import { NeptuneService } from '../shared/neptune/neptune.service';
+import { CreateBankTransactionDto } from './dto/create-bank-transaction.dto';
+import { UpdateBankTransactionDto } from './dto/update-bank-transaction.dto';
 
 describe('BankTransactionsService', () => {
   let service: BankTransactionsService;
+  let neptuneService: NeptuneService;
 
   beforeAll(() => {
     process.env.NEPTUNE_ENDPOINT_HOSTNAME = 'localhost';
@@ -17,9 +21,82 @@ describe('BankTransactionsService', () => {
     }).compile();
 
     service = module.get<BankTransactionsService>(BankTransactionsService);
+    neptuneService = module.get<NeptuneService>(NeptuneService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new bank transaction', async () => {
+    const createBankTransactionDto: CreateBankTransactionDto = {
+      otherPersonIBAN: 'DE1234567890',
+      amount: 1000,
+    };
+
+    jest
+      .spyOn(neptuneService, 'addVertex')
+      .mockResolvedValue(createBankTransactionDto);
+
+    const result = await service.create(createBankTransactionDto);
+    expect(result).toEqual(createBankTransactionDto);
+  });
+
+  it('should find all bank transactions', async () => {
+    const bankTransactions = [
+      { otherPersonIBAN: 'DE1234567890', amount: 1000 },
+      { otherPersonIBAN: 'DE0987654321', amount: 2000 },
+    ];
+
+    jest
+      .spyOn(neptuneService, 'findVertices')
+      .mockResolvedValue(bankTransactions);
+
+    const result = await service.findAll();
+    expect(result).toEqual(bankTransactions);
+  });
+
+  it('should find one bank transaction by transactionId', async () => {
+    const bankTransaction = { otherPersonIBAN: 'DE1234567890', amount: 1000 };
+
+    jest
+      .spyOn(neptuneService, 'findVertexByProperty')
+      .mockResolvedValue(bankTransaction);
+
+    const result = await service.findOne('transactionId');
+    expect(result).toEqual(bankTransaction);
+  });
+
+  it('should throw an error if bank transaction is not found', async () => {
+    jest.spyOn(neptuneService, 'findVertexByProperty').mockResolvedValue(null);
+
+    await expect(service.findOne('transactionId')).rejects.toThrow(
+      'BankTransaction not found',
+    );
+  });
+
+  it('should update a bank transaction', async () => {
+    const updateBankTransactionDto: UpdateBankTransactionDto = {
+      amount: 2000,
+    };
+
+    jest
+      .spyOn(neptuneService, 'updateVertex')
+      .mockResolvedValue('updatedVertexId');
+
+    const result = await service.update(
+      'transactionId',
+      updateBankTransactionDto,
+    );
+    expect(result).toEqual({ updatedVertexId: 'updatedVertexId' });
+  });
+
+  it('should remove a bank transaction', async () => {
+    jest
+      .spyOn(neptuneService, 'deleteVertex')
+      .mockResolvedValue('deletedVertexId');
+
+    const result = await service.remove('transactionId');
+    expect(result).toEqual({ deletedVertexId: 'deletedVertexId' });
   });
 });
